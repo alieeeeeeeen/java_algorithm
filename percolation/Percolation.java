5896ac7a9203bb13e15be07ebf3bcf53215304a6
alieeeeeeeen <elainechen77@icloud.com>
2021-07-08 15:46:14 +0800

    [fix] check if it is open

--- percolation/Percolation.java
+++ percolation/Percolation.java
@@ -8,7 +8,7 @@ public class Percolation {
     private int top;
     private int bottom;
     private boolean[] isOpened;
-    private int openCount = 0;
+    private int openCount;
 
     // creates n-by-n grid, with all sites initially blocked
     public Percolation(int n) {
@@ -18,46 +18,49 @@ public class Percolation {
         top = 0;
         size = n;
         bottom = size * size + 1;
-        isOpened = new boolean[n * n + 2];
+        isOpened = new boolean[size * size + 2];
         isOpened[top] = true;
         isOpened[bottom] = true;
-        backwashUF = new WeightedQuickUnionUF(n * n + 2); // 0 and last node as virtual sites
-        uf = new WeightedQuickUnionUF(n * n + 1);
+        openCount = 0;
+        backwashUF = new WeightedQuickUnionUF(size * size + 2); // 0 and last node as virtual sites
+        uf = new WeightedQuickUnionUF(size * size + 1);
     }
 
-    public int getIndexOf(int i, int j) {
-        if (i < 0 || i >= size) {
-            throw new IndexOutOfBoundsException();
+    private int getIndexOf(int i, int j) {
+        if (i < 1 || i > size) {
+            throw new IllegalArgumentException();
         }
-        if (j < 0 || j >= size) {
-            throw new IndexOutOfBoundsException();
+        if (j < 1 || j > size) {
+            throw new IllegalArgumentException();
         }
 
-        return i * size + j + 1; // get index of site
+        return (i - 1) * size + j; // get index of site
     }
 
     // opens the site (row, col) if it is not open already
     public void open(int i, int j) {
         int index = getIndexOf(i, j);
-        isOpened[index] = true;
-        openCount++;
-        if (i == 0) {
+        if (!isOpened[index]) {
+            isOpened[index] = true;
+            openCount++;
+        }
+        if (i == 1) {
             uf.union(index, top);
             backwashUF.union(index, top);
         }
-        if (i == size - 1) {
+        if (i == size) {
             backwashUF.union(index, bottom);
         }
-        if ((i - 1) >= 0 && (i - 1) < size && isOpen(i - 1, j)) {
+        if ((i - 1) > 0 && (i - 1) <= size && isOpen(i - 1, j)) {
             union(i, j, i - 1, j);
         }
-        if ((i + 1) >= 0 && (i + 1) < size && isOpen(i + 1, j)) {
+        if ((i + 1) > 0 && (i + 1) <= size && isOpen(i + 1, j)) {
             union(i, j, i + 1, j);
         }
-        if ((j - 1) >= 0 && (j - 1) < size && isOpen(i, j - 1)) {
+        if ((j - 1) > 0 && (j - 1) <= size && isOpen(i, j - 1)) {
             union(i, j, i, j - 1);
         }
-        if ((j + 1) >= 0 && (j + 1) < size  && isOpen(i, j + 1)) {
+        if ((j + 1) > 0 && (j + 1) <= size  && isOpen(i, j + 1)) {
             union(i, j, i, j + 1);
         }
     }
--- percolation/PercolationStats.java
+++ percolation/PercolationStats.java
@@ -6,24 +6,24 @@ import edu.princeton.cs.algs4.StdStats;
 
 public class PercolationStats {
     // perform independent trials on an n-by-n grid
-    private int n;
-    private int trials;
-    private double [] fractions;
+    private final int n;
+    private final int trials;
+    private double[] fractions;
     private final double constant = 1.96;
 
     public PercolationStats(int n, int trials) {
-        if(n <= 0 || trials <= 0) {
+        if (n <= 0 || trials <= 0) {
             throw new IllegalArgumentException();
         }
         this.n = n;
         this.trials = trials;
         fractions = new double[trials];
-        for(int i = 0; i < trials; i++) {
+        for (int i = 0; i < trials; i++) {
             Percolation percolation = new Percolation(n);
-            while(!percolation.percolates()) {
-                int no1 = StdRandom.uniform(n);
-                int no2 = StdRandom.uniform(n);
-                if(!percolation.isOpen(no1, no2)) {
+            while (!percolation.percolates()) {
+                int no1 = StdRandom.uniform(n) + 1;
+                int no2 = StdRandom.uniform(n) + 1;
+                if (!percolation.isOpen(no1, no2)) {
                     percolation.open(no1, no2);
                 }
             }
@@ -47,9 +47,7 @@ public class PercolationStats {
     }
 
     // high endpoint of 95% confidence interval
-    public double confidenceHi() {
-        return (mean() + constant * stddev() / Math.sqrt(trials));
-    }
+    public double confidenceHi() { return (mean() + constant * stddev() / Math.sqrt(trials)); }
 
     // test client (see below)
     public static void main(String[] args) {
