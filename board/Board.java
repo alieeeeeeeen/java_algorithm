32f9bbbe3c6f94415be5acd91677f864c884b12f
alieeeeeeeen <elainechen77@icloud.com>
2021-08-06 15:10:55 +0800

    [add] add board

--- board/Board.java
+++ board/Board.java
@@ -1,10 +1,15 @@
 package board;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Board {
     // create a board from an n-by-n array of tiles,
     // where tiles[row][col] = tile at (row, col)
     private final int dimension;
     private final int[][] tiles;
+    private int blankRow;
+    private int blankCol;
 
     public Board(int[][] tiles) {
         if (tiles == null) {
@@ -13,6 +18,15 @@ public class Board {
 
         this.tiles = tiles.clone();
         dimension = this.tiles.length;
+
+        for (int i = 0; i < dimension; i++) {
+            for (int j = 0; j < tiles[i].length; j++) {
+                if (tiles[i][j] == 0) {
+                    blankCol = j;
+                    blankRow = i;
+                }
+            }
+        }
     }
 
     // string representation of this board
@@ -48,7 +62,7 @@ public class Board {
                 if (tiles[i][j] != i * dimension + j + 1) {
                     int goalRow = tiles[i][j] / dimension;
                     int goalCol = tiles[i][j] % dimension;
-                    move += goalCol - j + goalRow - i;
+                    move += Math.abs(goalCol - j) + Math.abs(goalRow - i);
                 } else {
                     continue;
                 }
@@ -73,6 +87,14 @@ public class Board {
             return false;
         }
 
+        if (this.blankRow != that.blankRow) {
+            return false;
+        }
+
+        if (this.blankCol != that.blankCol) {
+            return false;
+        }
+
         for (int i = 0; i < dimension; i++) {
             for (int j = 0; j < tiles[i].length; j++) {
                  if (tiles[i][j] != that.tiles[i][j]) {
@@ -84,10 +106,43 @@ public class Board {
     }
 
     // all neighboring boards
-    public Iterable<Board> neighbors()
+    public Iterable<Board> neighbors() {
+        List<Board> neighbours = new ArrayList<>();
+        if (blankRow > 0) {
+            int [][] north = tiles.clone();
+            swap(north, blankRow, blankCol, blankRow - 1, blankCol);
+            neighbours.add(new Board(north));
+        }
+
+        if (blankRow < dimension - 1) {
+            int [][] south = tiles.clone();
+            swap(south, blankRow, blankCol, blankRow + 1, blankCol);
+            neighbours.add(new Board(south));
+        }
+
+        if (blankCol > 0) {
+            int [][] east = tiles.clone();
+            swap(east, blankRow, blankCol, blankRow, blankCol - 1);
+            neighbours.add(new Board(east));
+        }
+
+        if (blankCol < dimension - 1) {
+            int [][] west = tiles.clone();
+            swap(west, blankRow, blankCol, blankRow, blankCol + 1);
+            neighbours.add(new Board(west));
+        }
+
+        return neighbours;
+    }
 
     // a board that is obtained by exchanging any pair of tiles
     public Board twin() {
 
+    }
+
+    private void swap (int [][] v, int rowA, int colA, int rowB, int colB) {
+        int tmp = v[rowA][colA];
+        v[rowA][colA] = v[rowB][colB];
+        v[rowB][colB] = tmp;
     }
 }
