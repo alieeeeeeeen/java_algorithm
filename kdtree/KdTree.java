99df76a17b6abb9dab9c73a3fe55e4adcdccc851
alieeeeeeeen <elainechen77@icloud.com>
2021-08-11 14:53:20 +0800

    [add] add nearest

--- kdtree/KdTree.java
+++ kdtree/KdTree.java
@@ -153,7 +153,7 @@ public class KdTree {
         return result;
     }
 
-    public void rangeOver(Node node, RectHV rect, List<Point2D> result) {
+    private void rangeOver(Node node, RectHV rect, List<Point2D> result) {
         if (node == null) {
             return;
         }
@@ -174,15 +174,33 @@ public class KdTree {
     }
 
 //
-//    public Point2D nearest(Point2D p) {
-//        checkNull(p);
-//    }
-//
-//    private Point2D nearest(Point2D target, Point2D closest, Node node) {
-//        if (node == null) {
-//            return closest;
-//        }
-//    }
+    public Point2D nearest(Point2D p) {
+        checkNull(p);
+        return isEmpty() ? null : nearest(p, root.p, root);
+    }
+
+    private Point2D nearest(Point2D queryPoint, Point2D closest, Node node) {
+        if (node == null) {
+            return closest;
+        }
+
+        double closetPoint = closest.distanceTo(queryPoint);
+
+        if (closetPoint > node.rect.distanceTo(queryPoint)) {
+            double nodeDistance = node.p.distanceTo(queryPoint);
+
+            if (nodeDistance < closetPoint) closest = node.p;
+
+            if (node.isLeft(queryPoint)) {
+                closest = nearest(queryPoint, closest, node.left);
+                closest = nearest(queryPoint, closest, node.right);
+            } else {
+                closest = nearest(queryPoint, closest, node.right);
+                closest = nearest(queryPoint, closest, node.left);
+            }
+        }
+        return closest;
+    }
 
     private void checkNull(Object obj) {
         if (obj == null) {
@@ -213,80 +231,123 @@ public class KdTree {
 //        }
 //    }
 
-    public static void main(String[] args) {
-        // initialize the data structures from file
-        String filename = args[0];
-        In in = new In(filename);
-        PointSET brute = new PointSET();
-        KdTree kdtree = new KdTree();
-        while (!in.isEmpty()) {
-            double x = in.readDouble();
-            double y = in.readDouble();
-            Point2D p = new Point2D(x, y);
-            kdtree.insert(p);
-            brute.insert(p);
-        }
+//    public static void main(String[] args) {
+//        // initialize the data structures from file
+//        String filename = args[0];
+//        In in = new In(filename);
+//        PointSET brute = new PointSET();
+//        KdTree kdtree = new KdTree();
+//        while (!in.isEmpty()) {
+//            double x = in.readDouble();
+//            double y = in.readDouble();
+//            Point2D p = new Point2D(x, y);
+//            kdtree.insert(p);
+//            brute.insert(p);
+//        }
+//
+//        double x0 = 0.0, y0 = 0.0;      // initial endpoint of rectangle
+//        double x1 = 0.0, y1 = 0.0;      // current location of mouse
+//        boolean isDragging = false;     // is the user dragging a rectangle
+//
+//        // draw the points
+//        StdDraw.clear();
+//        StdDraw.setPenColor(StdDraw.BLACK);
+//        StdDraw.setPenRadius(0.01);
+//        brute.draw();
+//        StdDraw.show();
+//
+//        // process range search queries
+//        StdDraw.enableDoubleBuffering();
+//        while (true) {
+//
+//            // user starts to drag a rectangle
+//            if (StdDraw.isMousePressed() && !isDragging) {
+//                x0 = x1 = StdDraw.mouseX();
+//                y0 = y1 = StdDraw.mouseY();
+//                isDragging = true;
+//            }
+//
+//            // user is dragging a rectangle
+//            else if (StdDraw.isMousePressed() && isDragging) {
+//                x1 = StdDraw.mouseX();
+//                y1 = StdDraw.mouseY();
+//            }
+//
+//            // user stops dragging rectangle
+//            else if (!StdDraw.isMousePressed() && isDragging) {
+//                isDragging = false;
+//            }
+//
+//            // draw the points
+//            StdDraw.clear();
+//            StdDraw.setPenColor(StdDraw.BLACK);
+//            StdDraw.setPenRadius(0.01);
+//            brute.draw();
+//
+//            // draw the rectangle
+//            RectHV rect = new RectHV(Math.min(x0, x1), Math.min(y0, y1),
+//                    Math.max(x0, x1), Math.max(y0, y1));
+//            StdDraw.setPenColor(StdDraw.BLACK);
+//            StdDraw.setPenRadius();
+//            rect.draw();
+//
+////            // draw the range search results for brute-force data structure in red
+//            StdDraw.setPenRadius(0.03);
+//            StdDraw.setPenColor(StdDraw.RED);
+//            for (Point2D p : brute.range(rect))
+//                p.draw();
+//
+//            // draw the range search results for kd-tree in blue
+//            StdDraw.setPenRadius(0.02);
+//            StdDraw.setPenColor(StdDraw.BLUE);
+//            for (Point2D p : kdtree.range(rect))
+//                p.draw();
+//
+//            StdDraw.show();
+//            StdDraw.pause(20);
+//        }
+//    }
 
-        double x0 = 0.0, y0 = 0.0;      // initial endpoint of rectangle
-        double x1 = 0.0, y1 = 0.0;      // current location of mouse
-        boolean isDragging = false;     // is the user dragging a rectangle
-
-        // draw the points
-        StdDraw.clear();
-        StdDraw.setPenColor(StdDraw.BLACK);
-        StdDraw.setPenRadius(0.01);
-        brute.draw();
-        StdDraw.show();
-
-        // process range search queries
-        StdDraw.enableDoubleBuffering();
-        while (true) {
-
-            // user starts to drag a rectangle
-            if (StdDraw.isMousePressed() && !isDragging) {
-                x0 = x1 = StdDraw.mouseX();
-                y0 = y1 = StdDraw.mouseY();
-                isDragging = true;
-            }
-
-            // user is dragging a rectangle
-            else if (StdDraw.isMousePressed() && isDragging) {
-                x1 = StdDraw.mouseX();
-                y1 = StdDraw.mouseY();
-            }
-
-            // user stops dragging rectangle
-            else if (!StdDraw.isMousePressed() && isDragging) {
-                isDragging = false;
-            }
-
-            // draw the points
-            StdDraw.clear();
-            StdDraw.setPenColor(StdDraw.BLACK);
-            StdDraw.setPenRadius(0.01);
-            brute.draw();
-
-            // draw the rectangle
-            RectHV rect = new RectHV(Math.min(x0, x1), Math.min(y0, y1),
-                    Math.max(x0, x1), Math.max(y0, y1));
-            StdDraw.setPenColor(StdDraw.BLACK);
-            StdDraw.setPenRadius();
-            rect.draw();
-
-//            // draw the range search results for brute-force data structure in red
-            StdDraw.setPenRadius(0.03);
-            StdDraw.setPenColor(StdDraw.RED);
-            for (Point2D p : brute.range(rect))
-                p.draw();
-
-            // draw the range search results for kd-tree in blue
-            StdDraw.setPenRadius(0.02);
-            StdDraw.setPenColor(StdDraw.BLUE);
-            for (Point2D p : kdtree.range(rect))
-                p.draw();
-
-            StdDraw.show();
-            StdDraw.pause(20);
-        }
-    }
+//    public static void main(String[] args) {
+//        // initialize the two data structures with point from file
+//        String filename = args[0];
+//        In in = new In(filename);
+//        PointSET brute = new PointSET();
+//        KdTree kdtree = new KdTree();
+//        while (!in.isEmpty()) {
+//            double x = in.readDouble();
+//            double y = in.readDouble();
+//            Point2D p = new Point2D(x, y);
+//            kdtree.insert(p);
+//            brute.insert(p);
+//        }
+//
+//        // process nearest neighbor queries
+//        StdDraw.enableDoubleBuffering();
+//        while (true) {
+//
+//            // the location (x, y) of the mouse
+//            double x = StdDraw.mouseX();
+//            double y = StdDraw.mouseY();
+//            Point2D query = new Point2D(x, y);
+//
+//            // draw all of the points
+//            StdDraw.clear();
+//            StdDraw.setPenColor(StdDraw.BLACK);
+//            StdDraw.setPenRadius(0.01);
+//            brute.draw();
+//
+//            // draw in red the nearest neighbor (using brute-force algorithm)
+//            StdDraw.setPenRadius(0.03);
+//            StdDraw.setPenColor(StdDraw.RED);
+//            brute.nearest(query).draw();
+//            StdDraw.setPenRadius(0.02);
+//
+//            // draw in blue the nearest neighbor (using kd-tree algorithm)
+//            StdDraw.setPenColor(StdDraw.BLUE);
+//            kdtree.nearest(query).draw();
+//            StdDraw.show();
+//            StdDraw.pause(40);
+//        }
+//    }
 }
